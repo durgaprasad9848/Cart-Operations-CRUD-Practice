b072f973134484eb944fdafb3f0948ec2b647539
durgaprasad9848 <kanakadurgaprasad.p@gmail.com>
2023-04-19 17:13:35 +0530

    Updated by deleting unwanted files

--- sigmat/src/NavigationBar.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Navbar, Nav } from "react-bootstrap";
-
-function NavigationBar() {
-  return <div>hi prasad</div>;
-}
-
-export default NavigationBar;
